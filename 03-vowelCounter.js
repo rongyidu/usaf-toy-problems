a1de13003d09fb2046cbd616cb7625bf5621307a
Ashcoca <ashvincoca@gmail.com>
2020-02-26 10:11:31 -1000

    Adding master solution for vowelCounter

--- 03-vowelCounter.js
+++ 03-vowelCounter.js
@@ -13,8 +13,17 @@ vowelCounter("Hawaii")
 
  */
 
- var vowelCounter = function(string) {
-  // YOUR CODE HERE
+var vowelCounter = function(string) {
+  var count = 0;
+  var myString = string.toUpperCase();
+  // var stringArr = myString.split("");
+  var vowels = ["a","e","i","o","u"];
+  for (var i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
+      count ++;
+    }
+  }
+  return count;
 };
 
-// console.log(vowelCounter("Banana"))
+console.log(vowelCounter("Banana"))
