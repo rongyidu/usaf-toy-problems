6bd16115b2ab3ddb7f95ae30aa19b0d579d68a2e
Ashcoca <ashvincoca@gmail.com>
2020-02-26 06:21:45 -1000

    Adding day 3 problem

---
+++ 03-vowelCounter.js
@@ -0,0 +1,18 @@
+/*
+
+Write a function that, given a string, returns the number of vowels in that string.
+
+We will consider that a, e, i, o and u are vowels for the sake of this problem.
+
+Ex:
+
+vowelCounter("Banana")
+--> Returns 3
+vowelCounter("Hawaii")
+--> Returns 4
+
+ */
+
+ var vowelCounter = function(string) {
+   // YOUR CODE HERE
+ };
