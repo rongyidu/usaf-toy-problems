d8f19fa559581d14c430e84e017ee3a6519ac901
Ashcoca <ashvincoca@gmail.com>
2020-02-26 06:34:10 -1000

    re-adding problems

---
+++ 02-findOldestAge.js
@@ -0,0 +1,41 @@
+/*
+Given an array of objects of names and ages find the oldest age and return it.
+
+Example:
+
+var myArr = [
+  {
+    name: "Bill",
+    age: 34
+  },
+  {
+    name: "Derek",
+    age: 16
+  }
+];
+
+findOldestAge(myArr);
+--> returns 34
+
+*/
+
+
+
+
+var findOldestAge = function(array) {
+  // YOUR CODE HERE
+};
+
+// // Uncomment and copy into Chrome dev console to test your function!
+// var myArr = [
+//   {
+//     name: "Bill",
+//     age: 34
+//   },
+//   {
+//     name: "Derek",
+//     age: 16
+//   }
+// ];
+// console.log(findOldestAge(myArr));
+
